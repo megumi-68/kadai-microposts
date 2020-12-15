e8fc4b62b45126b29863c01d557f69fd7edc2e41
megumi.kimura <megu.megu.megu68@gmail.com>
2020-12-15 07:37:13 +0000

    favoriteee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show,:followings,:followers,:likes]
   def index
   @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -38,6 +38,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def likes
+    @user = User.find(params[:id])
+    @likes = @user.likes.page(params[:page])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
+  end
   private
 
   def user_params
